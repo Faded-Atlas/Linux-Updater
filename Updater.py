9002a21e6ac8c68e02a7742ea439d345b5cb97e6
Faded-Atlas <42407097+Faded-Atlas@users.noreply.github.com>
2019-11-10 22:45:09 -0500

    Update Updater.py
    Fixed Syntax Errors; Removed root password check.

--- Updater.py
+++ Updater.py
@@ -7,12 +7,13 @@ if os.geteuid() != 0:
     exit("Error, try running as root")
 
 a = input("Is running on a Raspberry Pi?")
-  if "yes" in a:
-    while True:print("This will update the raspberry pi")
-      sleep(5)
-      os.system("apt-get update && apt-get full-upgrade -y")
-      sleep(2)
-      os.system("uname -a")
+if "yes" in a:
+    while True:
+        print("This will update the raspberry pi")
+        sleep(5)
+        os.system("apt-get update && apt-get full-upgrade -y")
+        sleep(2)
+        os.system("uname -a")
 
-  else:
+else:
     exit("This most likely will not work on this device, I apologise")
